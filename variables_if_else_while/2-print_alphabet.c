8e709a539405adcfe2a129ed4f6e5ac850da2ef7
shootskull <alex.cerong@hotmail.com>
2022-06-14 09:25:59 -0700

    .

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - return the n value
+ * Return: return 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
