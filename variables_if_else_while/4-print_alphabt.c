9d0e16a82c847ce6d976a2032cf58d31add61304
shootskull <alex.cerong@hotmail.com>
2022-06-14 09:31:18 -0700

    .

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - return the n value
+ * Return: return 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' && ch != 'q')
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
