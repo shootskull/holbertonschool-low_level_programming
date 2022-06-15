5ca08b4b4b98eaad1b08e1f009169d34217073f4
shootskull <alex.cerong@hotmail.com>
2022-06-15 09:54:29 -0700

    .

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char c;
+
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char ch;
+
+i = 0;
+while (i < 10)
+{
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+i++;
+}
+}
